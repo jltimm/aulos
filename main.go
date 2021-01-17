bd91837d8f821f79f5887d0c57f093d7982621fb
jltimm <36246250+jltimm@users.noreply.github.com>
2021-01-17 15:39:50 -0600

    Adding shutdown hook (#5)

--- main.go
+++ main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
 	"./crawler"
 	"./io/postgres"
 	"./secrets"
 )
 
+func cleanup() {
+	fmt.Println("Shutting down...")
+}
+
 func main() {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		cleanup()
+		os.Exit(1)
+	}()
 	postgres.Initialize()
 	crawler.Crawl(secrets.GetSearchURL(0, 50))
 }
